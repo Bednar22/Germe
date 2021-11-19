0f7c7397aeb75f4689e2c4a7b293e8cc934af95f
Maciek Bednarski <mBednar2020@gmail.com>
2021-11-20 00:03:25 +0100

    dropdown in flashcard component

--- client/src/components/flagDropdown.js
+++ client/src/components/flagDropdown.js
@@ -1,10 +1,23 @@
 import germanflag from '../graphics/flag.png';
-
+import '../CSS/dropdown.css';
+import { Link } from 'react-router-dom';
 const FlagDropdown = () => {
     return (
         <>
-            <div className='flag-dropdown'>
-                <img src={germanflag} style={{ height: '30px' }}></img>
+            <div className='flag-dropdown-box'>
+                <div className='flag-dropdown'>
+                    <button className='link'>
+                        <img src={germanflag} alt={'Menu'} style={{ height: '30px' }} />{' '}
+                    </button>
+                    <div className='flag-dropdown-content'>
+                        <Link to='/gramma' className='flag-button'>
+                            GRAMMA
+                        </Link>
+                        <Link to='/culture' className='flag-button'>
+                            CULTURE
+                        </Link>
+                    </div>
+                </div>
             </div>
         </>
     );
--- client/src/components/gramma.js
+++ client/src/components/gramma.js
@@ -1,5 +1,17 @@
+import '../App.css';
+
 const Gramma = () => {
-    return;
+    return (
+        <>
+            <div className='content'>
+                <ul>
+                    <li className='mbut'>HOVER</li>
+                    <l1 className='expandable'> HAHA</l1>
+                    <l1 className='expandable'> HAHA</l1>
+                </ul>
+            </div>
+        </>
+    );
 };
 
 export default Gramma;
